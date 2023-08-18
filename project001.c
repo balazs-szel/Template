a021953b5b30243dff468d62c49027ce70d12793
sokakozos <sokakozos@gmail.com>
2023-08-18 16:14:44 +0200

    file storage function test_1

--- project001.c
+++ project001.c
@@ -1,27 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-int file (){
-int num;
-   FILE *fptr;
-
-   // use appropriate location if you are using MacOS or Linux
-   fptr = fopen("/Users/szelbalazs/Documents/VS Code/projetcs_code/Template/test_file.rtf","w");
-
-   if(fptr == NULL)
-   {
-      printf("Error!");   
-      exit(1);             
-   }
-
-   printf("Enter num: ");
-   scanf("%d",&num);
-
-   fprintf(fptr,"%d",num);
-   fclose(fptr);
-   return 0;
-};
-
+#include "file.h"
 
 int isPrime(int num) {
     if (num <= 1) {
@@ -41,6 +20,7 @@ void listPrimesBetween(int start, int end) {
     for (int num = start; num <= end; num++) {
         if (isPrime(num)) {
             printf("%d ", num);
+            void file_storage();
         }
     }
     
@@ -59,10 +39,5 @@ int main() {
     };
    
     listPrimesBetween(num1, num2);
-    
-    int file ();
-
-
-
     return 0;
 }
